79b2a8e9adac1e1104960ba06925006fae9e12e8
Giuliaqqrd <giulia.coucourde@gmail.com>
2024-06-07 11:45:48 +0200

    Last changes

--- company-service/src/main/java/com/backend/companyservice/repository/CompanyRepository.java
+++ company-service/src/main/java/com/backend/companyservice/repository/CompanyRepository.java
@@ -2,10 +2,11 @@ package com.backend.companyservice.repository;
 
 import com.backend.companyservice.model.Company;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 
 import java.util.List;
-
+@Repository
 public interface CompanyRepository extends MongoRepository<Company, String> {
     List<Company> findAllCompanyById(String id);
 
